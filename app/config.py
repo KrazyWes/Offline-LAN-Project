3d27da797503c2703891ebc38c4d0d7a6d8600ff
KrazyWes <wpl2022-5672-25946@bicol-u.edu.ph>
2026-01-30 10:15:59 +0800

    Updated contents

---
+++ app/config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# ON SERVER: Use "localhost"
+# ON CLIENT: Use "YOUR_COMPUTER_NAME.local" 
+DB_HOST = "localhost" 
+
+DB_NAME = "postgres"
+DB_USER = "postgres"
+DB_PASS = "1234"
+DB_PORT = "5432"
